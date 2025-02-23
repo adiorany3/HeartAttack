4e64c7c1f9fe887440bf7d7ce68bf05f16393217
adiorany3 <158135665+adiorany3@users.noreply.github.com>
2025-02-23 18:49:17 +0700

    hide everythink

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,3 +68,12 @@ st.markdown(f"""
     © {current_year} Developed by: Galuh Adi Insani. All rights reserved.
 </div>
 """, unsafe_allow_html=True)
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
